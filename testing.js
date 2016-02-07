214f3564910aa1538039a4d8e6b5a6dcb59ece5f
Clayton Boyle <clayton.boyle@gmail.com>
2016-02-07 15:58:39 -0700

    added marker appearing and removal

--- testing.js
+++ testing.js
@@ -24,12 +24,16 @@ app.controller("appController",["$scope",function($scope){
 	var s=$scope;
 
 	s.searchCity = "";
+        //below I create a new geocoder object
+    var geocoder = new google.maps.Geocoder();
+    //below I create a new marker obj, that is used to delete the previous person marker
+    var oldMarker = new google.maps.Marker({position:google.maps.LatLng(0,0)});
 
 	var map;
          function initMap() {
             map = new google.maps.Map(document.getElementById('map'), {
                center: {lat:39.7571357, lng: -105.028466},
-               zoom: 12
+               zoom: 11
             });
         }
 
@@ -49,16 +53,52 @@ app.controller("appController",["$scope",function($scope){
 		randLat = Math.random()*180 -90;
 		randLong = Math.random()*360 -180;
 		map.setCenter({lat:randLat, lng: randLong});
+        map.setZoom(11);
 		console.log(randLat,randLong);
 		
 
 	}	
+    
 
+    
 	s.consoleCity = function(){
 		console.log(s.searchCity);
+        //make below function an IIFE 
+        (function codeAddress() {
+            var address = s.searchCity;
+            
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                
+                console.log("Are we in here?");
+                
+                if (status == google.maps.GeocoderStatus.OK) {
+                        console.log(results[0].geometry.location);
+                        map.setCenter(results[0].geometry.location);
+                        map.setZoom(11);
+                    //remove the old marker
+                    oldMarker.setMap(null);
+                    //put the new position marker in
+                     var marker = new google.maps.Marker({
+                         position: results[0].geometry.location,
+                         title:"Hello World!"
+                        });
+                    //set oldmarker to the current marker
+                    oldMarker = marker;
+                    console.log(marker);
+                // To add the marker to the map, call setMap();
+                    marker.setMap(map);
+                } else {
+                        alert("Could not resolve the address.\n " + status);
+                }
+            });
+  }());
+
+        //reset the field
+        s.searchCity="";
 
 	}
 
+   
 
 }]);
 
